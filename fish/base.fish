419f15727fe149acb9dbb1378fb8d05b67829245
mucean <mocean.liu@gmail.com>
2021-05-23 23:18:32 +0800

    update

--- fish/base.fish
+++ fish/base.fish
@@ -2,12 +2,9 @@
 set -x COMPOSER_HOME "$HOME/.composer"
 
 set my_path \
+$my_path \
 "$HOME/.cargo/bin" \
 "$COMPOSER_HOME/vendor/bin" \
-
-if set -q mac_path
-    set my_path $my_path $mac_path
-end
 
 for p in $my_path;
     if test -d "$p"
@@ -24,12 +21,8 @@ bind \cf forward-word
 set -x LC_ALL en_US.UTF-8
 set -x DROPBOX_PATH "$HOME/Dropbox"
 set -x DATE_FORMAT '+%Y-%m-%d %H:%M:%S'
-#set -x GOPROXY "https://goproxy.io/"
 set -x GOPROXY "https://goproxy.cn/"
 
 if command -s powerline-daemon >/dev/null 2>&1 && test (count (ps -ef | grep powerline-daemon)) -ne 2
     powerline-daemon -q
 end
-
-#set -g fish_user_paths "/usr/local/opt/python@3.8/bin" $fish_user_paths
-#set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
