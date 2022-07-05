c6165d717afd1356fee4366e7ea8878d1e94f5cc
mucean <mocean.liu@gmail.com>
2022-07-05 18:51:30 +0800

    update

---
+++ leftwm/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+confDir="$HOME/.config/leftwm"
+if [ ! -e $confDir ]; then
+    mkdir -p $confDir;
+fi
+
+confThemeDir="$confDir/themes"
+if [ ! -e $confThemeDir ]; then
+    mkdir -p $confThemeDir;
+fi
+
+dir=$(pwd)
+
+if [ ! -e "$dir/config.toml" ]; then
+    ln -s "$dir/config.toml" "$confDir/config.toml"
+fi
+
+if [ ! -e "$confThemeDir/current" ]; then
+    ln -s "$dir/themes/default" "$confThemeDir/current"
+fi
