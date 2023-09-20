53ca8797e8a60f49bdbfcdc3a1a4b099401ee727
dylan <mocean.liu@gmail.com>
2023-09-20 13:21:52 +0800

    feat(core): update

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -43,6 +43,26 @@ local plugins = {
     opts = overrides.blankline,
   },
 
+  {
+  "NvChad/nvterm",
+    config = function ()
+      require("nvterm").setup({
+        terminals = {
+          type_opts = {
+            float = {
+              relative = "editor",
+              row = 0.1,
+              col = 0.1,
+              width = 0.8,
+              height = 0.75,
+              border = "single",
+            },
+          },
+        },
+      })
+    end,
+  },
+
   -- Install a plugin
   {
     "max397574/better-escape.nvim",
@@ -59,9 +79,6 @@ local plugins = {
     config = function()
       require("focus").setup({
         ui = {
-          number = true,
-          relativenumber = true,
-          hybridnumber = true,
           cursorline = true,
           cursorcolumn = true,
           colorcolumn = {enable = true, list = '+1,+2'},
