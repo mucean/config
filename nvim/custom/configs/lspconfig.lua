8e633fb2c8d5b4be0595ee53a500909606e22f80
mucean <mocean.liu@gmail.com>
2023-09-25 21:08:48 +0800

    update

--- nvim/custom/configs/lspconfig.lua
+++ nvim/custom/configs/lspconfig.lua
@@ -1,10 +1,11 @@
+local util = require 'lspconfig.util';
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "rust_analyzer", "pyright" }
+local servers = { "html", "cssls", "tsserver", "gopls", "rust_analyzer", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -12,6 +13,16 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig["clangd"].setup {
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig["bufls"].setup {
+  root_dir = util.root_pattern("buf.work.yaml", ".git", "buf.gen.yaml", "buf.yaml"),
+}
 
 -- 
 -- lspconfig.pyright.setup { blabla}
